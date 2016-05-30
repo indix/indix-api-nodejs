efd26e9067acc97b74649f8f23767a3969ec33af
praveenselvam <praveen.selvam@gmail.com>
2016-05-30 08:44:21 +0530

    Removing accidental comments

--- test/example.js
+++ test/example.js
@@ -6,97 +6,97 @@ ixClient.init({
   appKey: '378157877b09f59fd2aa2f3239438bc7'
 });
 
-// ixClient.getBrands({ q: 'Nike' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getStores({ q: 'Amazon.com' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getCategories().then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getSearchSuggestions({ q: 'red shoes', countryCode: 'US' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getProductSummary({ q: 'red shoes', countryCode: 'US' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getProductOffersStandard({ q: 'red shoes', countryCode: 'US' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getProductOffersPremium({ q: 'red shoes', countryCode: 'US' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getProductCatalogStandard({ q: 'red shoes', countryCode: 'US' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getProductCatalogPremium({ q: 'red shoes', countryCode: 'US' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getProductUniversal({ q: 'red shoes', countryCode: 'US' }).then(function(results){
-//   console.log('Got', results.length, 'results');
-// });
-//
-// ixClient.getProductLookupSummary({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
-//   console.log(result);
-// });
-//
-// ixClient.getProductLookupOffersStandard({ mpid: '059199349f7b05dcca38ddb7dc573b80', storeId: [24], countryCode: 'US' }).then(function(result){
-//   console.log(result);
-// });
-//
-// ixClient.getProductLookupOffersPremium({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
-//   console.log(result);
-// });
-//
-// ixClient.getProductLookupCatalogStandard({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
-//   console.log(result);
-// });
-//
-// ixClient.getProductLookupCatalogPremium({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
-//   console.log(result);
-// });
-//
-// ixClient.getProductLookupUniversal({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
-//   console.log(result);
-// });
-//
-// ixClient.getBulkProductSummary({
-//   q: 'nike',
-//   storeId: [24],
-//   startPrice: 367,
-//   endPrice: 367.01,
-//   countryCode: 'US'
-// }).then(function(result){
-//
-//   var poll = function(jobId){
-//     ixClient.getJobStatus(jobId).then(function(jobInfo){
-//       console.log(jobInfo);
-//       if(jobInfo.status != 'COMPLETED'){
-//         setTimeout(function(){
-//           poll(jobId);
-//         }, 5000);
-//       }else{
-//         ixClient.downloadProducts(jobId).then(function(products){
-//           console.log('done');
-//           console.log(products.length);
-//         });
-//       }
-//     });
-//   }
-//
-//   poll(result.id);
-//
-// });
+ixClient.getBrands({ q: 'Nike' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getStores({ q: 'Amazon.com' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getCategories().then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getSearchSuggestions({ q: 'red shoes', countryCode: 'US' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getProductSummary({ q: 'red shoes', countryCode: 'US' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getProductOffersStandard({ q: 'red shoes', countryCode: 'US' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getProductOffersPremium({ q: 'red shoes', countryCode: 'US' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getProductCatalogStandard({ q: 'red shoes', countryCode: 'US' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getProductCatalogPremium({ q: 'red shoes', countryCode: 'US' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getProductUniversal({ q: 'red shoes', countryCode: 'US' }).then(function(results){
+  console.log('Got', results.length, 'results');
+});
+
+ixClient.getProductLookupSummary({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
+  console.log(result);
+});
+
+ixClient.getProductLookupOffersStandard({ mpid: '059199349f7b05dcca38ddb7dc573b80', storeId: [24], countryCode: 'US' }).then(function(result){
+  console.log(result);
+});
+
+ixClient.getProductLookupOffersPremium({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
+  console.log(result);
+});
+
+ixClient.getProductLookupCatalogStandard({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
+  console.log(result);
+});
+
+ixClient.getProductLookupCatalogPremium({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
+  console.log(result);
+});
+
+ixClient.getProductLookupUniversal({ mpid: '059199349f7b05dcca38ddb7dc573b80', countryCode: 'US' }).then(function(result){
+  console.log(result);
+});
+
+ixClient.getBulkProductSummary({
+  q: 'nike',
+  storeId: [24],
+  startPrice: 367,
+  endPrice: 367.01,
+  countryCode: 'US'
+}).then(function(result){
+
+  var poll = function(jobId){
+    ixClient.getJobStatus(jobId).then(function(jobInfo){
+      console.log(jobInfo);
+      if(jobInfo.status != 'COMPLETED'){
+        setTimeout(function(){
+          poll(jobId);
+        }, 5000);
+      }else{
+        ixClient.downloadProducts(jobId).then(function(products){
+          console.log('done');
+          console.log(products.length);
+        });
+      }
+    });
+  }
+
+  poll(result.id);
+
+});
 
 ixClient.getBulkProductLookupSummary({
   inputFile: fs.createReadStream('test/stubs/bulk-job-input.txt', 'utf8'),
