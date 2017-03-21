ec9ad0d8a2429c94e32e9b2785af09a0ee46eec0
Raja <bethleharaja@indix.com>
2017-03-21 19:13:58 +0530

    Ixe 8279 (#13)
    * version bump 0.2.1
    
    * IXE-8279: Review comments incorporated
    
    * Test cases added
    
    * Stub data corrected
    
    * typo in the unti-test title corrected

--- test/product-search-test.js
+++ test/product-search-test.js
@@ -59,6 +59,98 @@ describe('Product Search', function() {
 
   });
 
+  it('summary should return appropriate products from store/s or brand/s 1', function(done){
+
+    var stubData = JSON.parse(fs.readFileSync('test/stubs/product-search-summary.json', 'utf8'));
+    nock('https://api.indix.com').get('/v2/summary/products?q=nike&storeId=24&countryCode=US&app_id=APP_ID&app_key=APP_KEY').reply(200, stubData);
+
+    var query = {
+      q: 'nike',
+      storeId: '24',
+      countryCode: 'US'
+    };
+
+    ixClient.getProductSummary(query).then(function(results){
+      results.length.should.equal(10);
+      results[0].should.be.like({
+        "categoryNamePath":"Shoes > Men > Fashion Sneakers",
+        "categoryId":20082,
+        "mpid":"8e1d8cd338ada38624d2f9322b259402",
+        "categoryName":"Fashion Sneakers",
+        "upcs":[
+          "00884499652748",
+          "00091206081795",
+          "00091206081818",
+          "00884499652755"
+        ],
+        "brandName":"Nike",
+        "minSalePrice":45.02,
+        "brandId":5572,
+        "categoryIdPath":"10179 > 17421 > 20082",
+        "mpns":[
+          "ROSHERUN",
+          "511881-405"
+        ],
+        "countryCode":"US",
+        "currency":"USD",
+        "title":"Nike Men's Rosherun Running Shoe.",
+        "lastRecordedAt":1462486236868,
+        "imageUrl":"http://ecx.images-amazon.com/images/I/41N3coiimrL._AA200_.jpg",
+        "maxSalePrice":162.0,
+        "offersCount":212,
+        "storesCount":1
+      });
+      done();
+    });
+
+  });
+
+  it('summary should return appropriate products from store/s or brand/s 2', function(done){
+
+    var stubData = JSON.parse(fs.readFileSync('test/stubs/product-search-summary.json', 'utf8'));
+    nock('https://api.indix.com').get('/v2/summary/products?q=nike&storeId=24&storeId=25&countryCode=US&app_id=APP_ID&app_key=APP_KEY').reply(200, stubData);
+
+    var query = {
+      q: 'nike',
+      storeId: ['24', '25'],
+      countryCode: 'US'
+    };
+
+    ixClient.getProductSummary(query).then(function(results){
+      results.length.should.equal(10);
+      results[0].should.be.like({
+        "categoryNamePath":"Shoes > Men > Fashion Sneakers",
+        "categoryId":20082,
+        "mpid":"8e1d8cd338ada38624d2f9322b259402",
+        "categoryName":"Fashion Sneakers",
+        "upcs":[
+          "00884499652748",
+          "00091206081795",
+          "00091206081818",
+          "00884499652755"
+        ],
+        "brandName":"Nike",
+        "minSalePrice":45.02,
+        "brandId":5572,
+        "categoryIdPath":"10179 > 17421 > 20082",
+        "mpns":[
+          "ROSHERUN",
+          "511881-405"
+        ],
+        "countryCode":"US",
+        "currency":"USD",
+        "title":"Nike Men's Rosherun Running Shoe.",
+        "lastRecordedAt":1462486236868,
+        "imageUrl":"http://ecx.images-amazon.com/images/I/41N3coiimrL._AA200_.jpg",
+        "maxSalePrice":162.0,
+        "offersCount":212,
+        "storesCount":1
+      });
+      done();
+    });
+
+  });
+
   it('offers standard should return the approppriate products with offers standard information', function(done){
 
     var stubData = JSON.parse(fs.readFileSync('test/stubs/product-search-offers-standard.json', 'utf8'));
